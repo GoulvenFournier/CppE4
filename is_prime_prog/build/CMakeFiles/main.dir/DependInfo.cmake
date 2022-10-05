09950a7886133552f270609f874f3f7b9c21f56e
GoulvenFournier <114063965+GoulvenFournier@users.noreply.github.com>
2022-10-05 10:32:56 +0200

    Add files via upload

---
+++ is_prime_prog/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/benji/OneDrive/Documents/ESIEE/C++ E4/TD1/isPrime.cpp" "/mnt/c/Users/benji/OneDrive/Documents/ESIEE/C++ E4/TD1/build/CMakeFiles/main.dir/isPrime.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
